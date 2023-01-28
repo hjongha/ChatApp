e98b63fd748e37686b537825afd1b65afffe0590
hjongha <90365579+hjongha@users.noreply.github.com>
2023-01-28 16:33:19 +0900

    Add files via upload

--- app/src/main/java/com/example/chatapplication/ChatActivity.java
+++ app/src/main/java/com/example/chatapplication/ChatActivity.java
@@ -1,18 +1,28 @@
 package com.example.chatapplication;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Hashtable;
 
@@ -35,13 +45,40 @@ public class ChatActivity extends AppCompatActivity {
         layoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(layoutManager);
 
-        String[] myDataset = {"1111111111", "2222222222", "3333333333", "4444444444"};
-        mAdapter = new ChatAdapter(myDataset);
+        ArrayList<ChatData> chatDataArrayList = new ArrayList<>();
+        mAdapter = new ChatAdapter(chatDataArrayList);
         recyclerView.setAdapter(mAdapter);
+
+        ChildEventListener childEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                ChatData chatData = snapshot.getValue(ChatData.class);
+                /*String date = snapshot.getKey();
+                String[] strings = new String[2];
+                strings = date.split(" ");
+                if (strings[0].equals())*/
+                chatDataArrayList.add(chatData);
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) { }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) { }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) { }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) { }
+        };
+        myRef.addChildEventListener(childEventListener);
 
         Button btn_send = (Button) findViewById(R.id.btn_send);
         EditText editText = (EditText) findViewById(R.id.editText);
 
+        // 전송 버튼 수행
         btn_send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
---
+++ app/src/main/java/com/example/chatapplication/login/UserAccount.java
@@ -0,0 +1,24 @@
+package com.example.chatapplication.login;
+
+public class UserAccount {
+    private String Uid;
+    private String email;
+    private String password;
+    private String name;
+
+    public String getUid() {return Uid;}
+
+    public void setUid(String uid) {Uid = uid;}
+
+    public String getEmail() {return email;}
+
+    public void setEmail(String email) {this.email = email;}
+
+    public String getPassword() {return password;}
+
+    public void setPassword(String password) {this.password = password;}
+
+    public String getName() {return name;}
+
+    public void setName(String name) {this.name = name;}
+}
